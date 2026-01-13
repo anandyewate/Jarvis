bc36e076ec48ba3ddadd4b1de59a65ff0facd684
anandyewate <anand.rao1708@gmail.com>
2026-01-13 10:50:02 +0530

    Update API key in client.py

--- Jarvis/client.py
+++ Jarvis/client.py
@@ -1,7 +1,8 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAwHM-6NiUM9_yC4n7AmbGIxJZxmF2CxyQ")
+genai.configure(api_key="API")
 model = genai.GenerativeModel('gemini-pro')
 
 response = model.generate_content("What is coding?")
 print(response.text)
+
